93c521b285c8ba2c46b32c303b44570cc8a20e05
Daniel Duton <daniel@duton.lu>
2020-09-28 11:42:59 +0200

    add ws server

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -1,8 +1,13 @@
 import Koa from 'koa'
+import WebSocket from 'ws'
+import http from 'http'
+
 const app = new Koa()
+const server = http.createServer(app.callback())
+const wss = new WebSocket.Server({ server })
 
 app.use(async ctx => {
     ctx.body = 'Hello World'
 })
 
-app.listen(3000)
+server.listen(3000)
